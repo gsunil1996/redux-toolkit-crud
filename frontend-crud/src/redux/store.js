ff0c935414388efcad4b30fc8026966973888f84
gsunil1996 <gajulasunilkumar123@gmail.com>
2024-01-20 13:12:37 +0530

    update

--- frontend-crud/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import employeeTableReducer from "./features/employeeTableSlice";
-
-export const store = configureStore({
-  reducer: {
-    employees: employeeTableReducer,
-  },
-  devTools: true,
-});
